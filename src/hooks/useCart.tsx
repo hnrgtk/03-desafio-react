91007448d7c7248cac1ff2b3e4a0dcdd1f0389b6
Henry Gabriel <hnrtk@hotmail.com>
2022-02-11 11:06:57 -0300

    challenge finished

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -21,30 +21,71 @@ interface CartContextData {
 
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
+const LOCAL_STORAGE_KEY = "@RocketShoes:cart";
+
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const { data } = await api.get(`stock/${productId}`);
+      const stock = data.amount;
+
+      const cartCopy = [...cart];
+
+      const productExistsInCart = cartCopy.find(
+        (product) => product.id === productId
+      );
+      const currentAmount = productExistsInCart
+        ? productExistsInCart.amount + 1
+        : 1;
+
+      if (currentAmount > stock) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      if (productExistsInCart) {
+        productExistsInCart.amount = currentAmount;
+      } else {
+        const { data } = await api.get(`products/${productId}`);
+        cartCopy.push({
+          ...data,
+          amount: 1,
+        });
+      }
+
+      setCart(cartCopy);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartCopy));
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const cartCopy = [...cart];
+      const productIndex = cartCopy.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (productIndex >= 0) {
+        cartCopy.splice(productIndex, 1);
+        setCart(cartCopy);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartCopy));
+      } else {
+        toast.error("Erro na remoção do produto");
+      }
     } catch {
-      // TODO
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +94,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) return;
+
+      const { data } = await api.get(`stock/${productId}`);
+      const stock = data.amount;
+
+      if (amount > stock) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const cartCopy = [...cart];
+      const productExistsInCart = cartCopy.find(
+        (product) => product.id === productId
+      );
+
+      if (productExistsInCart) {
+        productExistsInCart.amount = amount;
+        setCart(cartCopy);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartCopy));
+      } else {
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import {
   MdDelete,
   MdAddCircleOutline,
   MdRemoveCircleOutline,
-} from 'react-icons/md';
+} from "react-icons/md";
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
-import { Container, ProductTable, Total } from './styles';
+import { useCart } from "../../hooks/useCart";
+import { formatPrice } from "../../util/format";
+import { Container, ProductTable, Total } from "./styles";
 
 interface Product {
   id: number;
@@ -17,29 +16,40 @@ interface Product {
   amount: number;
 }
 
+type UpdateProductType = Pick<Product, "id" | "amount">;
+
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
+  const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const cartFormatted = cart.map((product) => ({
+    ...product,
+    priceFormatted: formatPrice(product.price),
+    subTotal: formatPrice(product.price * product.amount),
+  }));
 
-  function handleProductIncrement(product: Product) {
-    // TODO
+  const total = formatPrice(
+    cart.reduce(
+      (sumTotal, product) => sumTotal + product.price * product.amount,
+      0
+    )
+  );
+
+  function handleProductIncrement({ amount, id }: UpdateProductType) {
+    updateProductAmount({
+      productId: id,
+      amount: amount + 1,
+    });
   }
 
-  function handleProductDecrement(product: Product) {
-    // TODO
+  function handleProductDecrement({ amount, id }: UpdateProductType) {
+    updateProductAmount({
+      productId: id,
+      amount: amount - 1,
+    });
   }
 
   function handleRemoveProduct(productId: number) {
-    // TODO
+    removeProduct(productId);
   }
 
   return (
@@ -55,52 +65,66 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          <tr data-testid="product">
-            <td>
-              <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-            </td>
-            <td>
-              <strong>Tênis de Caminhada Leve Confortável</strong>
-              <span>R$ 179,90</span>
-            </td>
-            <td>
-              <div>
-                <button
-                  type="button"
-                  data-testid="decrement-product"
-                // disabled={product.amount <= 1}
-                // onClick={() => handleProductDecrement()}
-                >
-                  <MdRemoveCircleOutline size={20} />
-                </button>
-                <input
-                  type="text"
-                  data-testid="product-amount"
-                  readOnly
-                  value={2}
-                />
-                <button
-                  type="button"
-                  data-testid="increment-product"
-                // onClick={() => handleProductIncrement()}
-                >
-                  <MdAddCircleOutline size={20} />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ 359,80</strong>
-            </td>
-            <td>
-              <button
-                type="button"
-                data-testid="remove-product"
-              // onClick={() => handleRemoveProduct(product.id)}
-              >
-                <MdDelete size={20} />
-              </button>
-            </td>
-          </tr>
+          {cartFormatted.map(
+            ({ image, title, amount, id, subTotal, price, priceFormatted }) => (
+              <tr data-testid="product" key={id}>
+                <td>
+                  <img src={image} alt={title} />
+                </td>
+                <td>
+                  <strong>{title}</strong>
+                  <span>{priceFormatted}</span>
+                </td>
+                <td>
+                  <div>
+                    <button
+                      type="button"
+                      data-testid="decrement-product"
+                      disabled={amount <= 1}
+                      onClick={() =>
+                        handleProductDecrement({
+                          amount,
+                          id,
+                        })
+                      }
+                    >
+                      <MdRemoveCircleOutline size={20} />
+                    </button>
+                    <input
+                      type="text"
+                      data-testid="product-amount"
+                      readOnly
+                      value={amount}
+                    />
+                    <button
+                      type="button"
+                      data-testid="increment-product"
+                      onClick={() =>
+                        handleProductIncrement({
+                          amount,
+                          id,
+                        })
+                      }
+                    >
+                      <MdAddCircleOutline size={20} />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>{subTotal}</strong>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="remove-product"
+                    onClick={() => handleRemoveProduct(id)}
+                  >
+                    <MdDelete size={20} />
+                  </button>
+                </td>
+              </tr>
+            )
+          )}
         </tbody>
       </ProductTable>
 
@@ -109,7 +133,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
